119b8c5887df4f00729af9664e2fc6579dcbbe2e
Marco Venturini <40200520+ventu8746@users.noreply.github.com>
2021-07-17 08:53:28 +0200

    implement function additem

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,109 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+// import React, { useEffect } from "react";
+import { addItem } from "./function/function";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [inputText, setInput] = useState("");
+	const [todos, setTodos] = useState([]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+	return (
+		<div>
+			<head>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>Template • TodoMVC</title>
+				<link rel="stylesheet" href="node_modules/todomvc-common/base.css" />
+				<link rel="stylesheet" href="node_modules/todomvc-app-css/index.css" />
+
+				<link rel="stylesheet" href="css/app.css" />
+			</head>
+			<body>
+				<section className="todoapp">
+					<header className="header">
+						<h1>TodoAppMarco</h1>
+						<form
+							onSubmit={(e) => {
+								e.preventDefault();
+								setTodos(addItem(todos, inputText));
+							}}
+						>
+							<input
+								className="new-todo"
+								onInput={(evt) => setInput(evt.target.value)}
+								placeholder="What needs to be done?"
+								autofocus
+							/>
+						</form>
+					</header>
+					{/* <!-- This section should be hidden by default and shown when there are todos --> */}
+					<section className="main">
+						<input id="toggle-all" className="toggle-all" type="checkbox" />
+						<label for="toggle-all">Mark all as complete</label>
+						<ul className="todo-list">
+							{/* <!-- These are here just to show the structure of the list items -->
+					<!-- List items should get the className `editing` when editing and `completed` when marked as completed --> */}
+							<li className="completed">
+								<div className="view">
+									<input className="toggle" type="checkbox" checked />
+									<label>Taste JavaScript</label>
+									<button className="destroy"></button>
+								</div>
+								<input className="edit" value="Create a TodoMVC template" />
+							</li>
+							<li>
+								<div className="view">
+									<input className="toggle" type="checkbox" />
+									<label>Buy a unicorn</label>
+									<button className="destroy"></button>
+								</div>
+								<input className="edit" value="Rule the web" />
+							</li>
+						</ul>
+					</section>
+					{/* <!-- This footer should be hidden by default and shown when there are todos --> */}
+					<footer className="footer">
+						{/* <!-- This should be `0 items left` by default --> */}
+						<span className="todo-count">
+							<strong>0</strong> item left
+						</span>
+						{/* <!-- Remove this if you don't implement routing --> */}
+						<ul className="filters">
+							<li>
+								<a className="selected" href="#/">
+									All
+								</a>
+							</li>
+							<li>
+								<a href="#/active">Active</a>
+							</li>
+							<li>
+								<a href="#/completed">Completed</a>
+							</li>
+						</ul>
+						{/* <!-- Hidden if no completed items are left ↓ --> */}
+						<button className="clear-completed">Clear completed</button>
+					</footer>
+				</section>
+				<footer className="info">
+					<p>Double-click to edit a todo</p>
+					{/* <!-- Remove the below line ↓ --> */}
+					<p>
+						Template by <a href="http://sindresorhus.com">Sindre Sorhus</a>
+					</p>
+					{/* <!-- Change this out with your name and url ↓ --> */}
+					<p>
+						Created by <a href="http://todomvc.com">you</a>
+					</p>
+					<p>
+						Part of <a href="http://todomvc.com">TodoMVC</a>
+					</p>
+				</footer>
+				{/* <!-- Scripts here. Don't remove ↓ --> */}
+				<script src="node_modules/todomvc-common/base.js"></script>
+				<script src="js/app.js"></script>
+			</body>
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/function/function.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+/**
+ *
+ * @param {Array<{text: string, completed: boolean}>} arr
+ * @param {string} string
+ * @returns {Array<{text: string, completed: boolean}>}s
+ */
+export function addItem(arr, string) {
+	if (string === "") {
+		return arr;
+	}
+	return arr.concat([
+		{
+			todo: string,
+			completed: false,
+		},
+	]);
+}
+
+/**
+ *
+ * @param {Array<{text: string, completed: boolean}>} arr
+ * @param {number} index
+ * @returns {Array<{text: string, completed: boolean}>
+ */
+export function deleteTodo(arr, index) {
+	if (index > arr.length || index < 0) {
+		throw new Error("the index that you have insert is inexistent");
+	}
+
+	if (index === "" || index === undefined) {
+		return arr;
+	} else {
+		return arr.filter((elem) => {
+			return elem !== arr[index];
+		});
+	}
+}
+
+/**
+ *
+ * @param {Array<{text: string, completed: boolean}>} arr
+ * @param {number} index
+ * @returns {Array<{text: string, completed: boolean}>
+ */
+export function toggleTodo(arr, index) {
+	if (index > arr.length || index < 0) {
+		throw new Error("the index that you have insert is inexistent");
+	}
+
+	if (index === "" || index === undefined) {
+		return arr;
+	} else {
+		return arr.map((elem, index2) => {
+			if (index !== index2) {
+				return elem;
+			} else {
+				return { todo: elem.todo, completed: !elem.completed };
+			}
+		});
+	}
+}
