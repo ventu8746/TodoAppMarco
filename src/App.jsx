8e33bac61170c9cf53befb83bb04d003f4aab40f
Marco Venturini <40200520+ventu8746@users.noreply.github.com>
2021-07-17 14:51:01 +0200

    implement checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -8,100 +8,98 @@ function App() {
 
 	return (
 		<div>
-			<head>
-				<meta charset="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>Template • TodoMVC</title>
-				<link rel="stylesheet" href="node_modules/todomvc-common/base.css" />
-				<link rel="stylesheet" href="node_modules/todomvc-app-css/index.css" />
+			<section className="todoapp">
+				<header className="header">
+					<h1>TodoAppMarco</h1>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							setTodos(addItem(todos, inputText));
 
-				<link rel="stylesheet" href="css/app.css" />
-			</head>
-			<body>
-				<section className="todoapp">
-					<header className="header">
-						<h1>TodoAppMarco</h1>
-						<form
-							onSubmit={(e) => {
-								e.preventDefault();
-								setTodos(addItem(todos, inputText));
-							}}
-						>
-							<input
-								className="new-todo"
-								onInput={(evt) => setInput(evt.target.value)}
-								placeholder="What needs to be done?"
-								autofocus
-							/>
-						</form>
-					</header>
-					{/* <!-- This section should be hidden by default and shown when there are todos --> */}
-					<section className="main">
-						<input id="toggle-all" className="toggle-all" type="checkbox" />
-						<label for="toggle-all">Mark all as complete</label>
-						<ul className="todo-list">
-							{/* <!-- These are here just to show the structure of the list items -->
+							console.log(todos);
+						}}
+					>
+						<input
+							className="new-todo"
+							onInput={(evt) => setInput(evt.target.value)}
+							placeholder="What needs to be done?"
+							autoFocus
+						/>
+					</form>
+				</header>
+				{/* <!-- This section should be hidden by default and shown when there are todos --> */}
+				<section className="main">
+					<input id="toggle-all" className="toggle-all" type="checkbox" />
+					<label htmlFor="toggle-all">Mark all as complete</label>
+					<ul className="todo-list">
+						{/* <!-- These are here just to show the structure of the list items -->
 					<!-- List items should get the className `editing` when editing and `completed` when marked as completed --> */}
-							<li className="completed">
+						{todos.map((todo, index) => (
+							<li className={todo.completed ? "completed" : ""}>
 								<div className="view">
-									<input className="toggle" type="checkbox" checked />
-									<label>Taste JavaScript</label>
+									<input
+										onClick={(e) =>
+											e.target.checked ? !e.target.checked  : e.target.checked
+											todo.completed ? !todo.completed
+										}
+										className="toggle"
+										type="checkbox"
+									/>
+									<label>{todo.todo}</label>
 									<button className="destroy"></button>
 								</div>
-								<input className="edit" value="Create a TodoMVC template" />
+								{<input className="edit" value="Create a TodoMVC template" />}
 							</li>
-							<li>
-								<div className="view">
-									<input className="toggle" type="checkbox" />
-									<label>Buy a unicorn</label>
-									<button className="destroy"></button>
-								</div>
-								<input className="edit" value="Rule the web" />
-							</li>
-						</ul>
-					</section>
-					{/* <!-- This footer should be hidden by default and shown when there are todos --> */}
-					<footer className="footer">
-						{/* <!-- This should be `0 items left` by default --> */}
-						<span className="todo-count">
-							<strong>0</strong> item left
-						</span>
-						{/* <!-- Remove this if you don't implement routing --> */}
-						<ul className="filters">
-							<li>
-								<a className="selected" href="#/">
-									All
-								</a>
-							</li>
-							<li>
-								<a href="#/active">Active</a>
-							</li>
-							<li>
-								<a href="#/completed">Completed</a>
-							</li>
-						</ul>
-						{/* <!-- Hidden if no completed items are left ↓ --> */}
-						<button className="clear-completed">Clear completed</button>
-					</footer>
+						))}
+						<li>
+							<div className="view">
+								<input className="toggle" type="checkbox" />
+								<label>Buy a unicorn</label>
+								<button className="destroy"></button>
+							</div>
+							<input className="edit" value="Rule the web" />
+						</li>
+					</ul>
 				</section>
-				<footer className="info">
-					<p>Double-click to edit a todo</p>
-					{/* <!-- Remove the below line ↓ --> */}
-					<p>
-						Template by <a href="http://sindresorhus.com">Sindre Sorhus</a>
-					</p>
-					{/* <!-- Change this out with your name and url ↓ --> */}
-					<p>
-						Created by <a href="http://todomvc.com">you</a>
-					</p>
-					<p>
-						Part of <a href="http://todomvc.com">TodoMVC</a>
-					</p>
+				{/* <!-- This footer should be hidden by default and shown when there are todos --> */}
+				<footer className="footer">
+					{/* <!-- This should be `0 items left` by default --> */}
+					<span className="todo-count">
+						<strong>0</strong> item left
+					</span>
+					{/* <!-- Remove this if you don't implement routing --> */}
+					<ul className="filters">
+						<li>
+							<a className="selected" href="#/">
+								All
+							</a>
+						</li>
+						<li>
+							<a href="#/active">Active</a>
+						</li>
+						<li>
+							<a href="#/completed">Completed</a>
+						</li>
+					</ul>
+					{/* <!-- Hidden if no completed items are left ↓ --> */}
+					<button className="clear-completed">Clear completed</button>
 				</footer>
-				{/* <!-- Scripts here. Don't remove ↓ --> */}
-				<script src="node_modules/todomvc-common/base.js"></script>
-				<script src="js/app.js"></script>
-			</body>
+			</section>
+			<footer className="info">
+				<p>Double-click to edit a todo</p>
+				{/* <!-- Remove the below line ↓ --> */}
+				<p>
+					Template by <a href="http://sindresorhus.com">Sindre Sorhus</a>
+				</p>
+				{/* <!-- Change this out with your name and url ↓ --> */}
+				<p>
+					Created by <a href="http://todomvc.com">you</a>
+				</p>
+				<p>
+					Part of <a href="http://todomvc.com">TodoMVC</a>
+				</p>
+			</footer>
+			{/* <!-- Scripts here. Don't remove ↓ --> */}
 		</div>
 	);
 }
